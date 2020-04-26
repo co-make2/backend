693bce4cd76a65c2e2206be8ea2e61bdaba0556f
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-26 16:41:39 -0600

    ready to test the post for user

--- users/users-router.js
+++ users/users-router.js
@@ -8,7 +8,37 @@ const Users = require('./users-model.js');
 
 //CRUDs Here!!! start with /api/users
 
+router.get('/', (req, res) => {
+    Users.find()
+      .then(users => {
+          res.status(200).json(users)
+      })
+      .catch(error => {
+          res.status(500).json({ message: "failed to pull users from server", error: error.message})
+      })
+})
 
+router.post("/register", (req, res) => {
+    const userData = req.body
+    const rounds = process.env.HASH_ROUNDS || 6;
+    const hash = bcrypt.hashSync(userData.password, rounds)
+    
+    userData.password = hash
+
+    if(userData.username && userData.email && username.password && username.zip){
+        userData.id = shortId.generate();
+        Users.add(userData)
+          .then(newUser => {
+              const token = gt.generateToken(userData)
+              res.status(201).json({newUser, token, message: "user Created"})
+          })
+          .catch(error => {
+            res.status(500).json({message: `Server Failed to Create a new User ${error.message}`})
+          })
+    }else{
+        res.status(500).json({errorMessage: "The necessary information to create a user was not provided"})
+    }
+})
 
 
 
