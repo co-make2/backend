4ea39691b775ed6deddd89edfb0797a1628eac7e
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-27 14:46:23 -0600

    posts router now has regular get working along with the post CRUD

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,8 +2,28 @@ const router = require('express').Router();
 
 const Posts = require('./posts-model.js');
 
+router.get('/', (req, res) => {
+    Posts.find()
+      .then(posts => {
+          res.status(200).json(posts)
+      })
+      .catch(error => {
+          res.status(500).json({ message: "failed to pull posts from server", error: error.message})
+      })
+})
 
 
+router.post('/', (req, res) => {
+    postData = req.body
+
+    Posts.add(postData)
+      .then(newPost => {
+          res.status(201).json(newPost)
+      })
+      .catch(error => {
+        res.status(500).json({message: "server Failed to create a post", error: error.message})
+    })
+})
 
 
 
