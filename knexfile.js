333484279fe729b9e928021e6043c896d87f676f
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-27 18:51:11 -0600

    updating knexfile for production to take into account foreign keys

--- knexfile.js
+++ knexfile.js
@@ -43,7 +43,11 @@ module.exports = {
     },
     seeds: {
       directory: './data/seeds',
-    }
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
   }
 
 };
