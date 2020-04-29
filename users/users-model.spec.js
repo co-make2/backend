420e9be66b6c07ab1efd285e68b3909b2c6b4383
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-29 13:44:24 -0600

    added test for register on user

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -4,7 +4,7 @@ const server = require('../api/server.js')
 const db = require('../data/dbConfig.js');
 
 describe('users router', function (){
-    describe('GET /', function () {
+    describe('GET /api/users', function () {
         it('should return 200 OK', function (){
             return request(server)
               .get('/api/users')
@@ -14,8 +14,30 @@ describe('users router', function (){
         })
     })
 
+    const testUser = {
+        username: "testuser",
+        email: "test@email.com",
+        password: "testpassword",
+        zip: "00000"
+    }
+
+    describe('POST /api/users/register', function () {
+        beforeEach(async () => {
+            await db('users').truncate() 
+        })
+        
+        it("returns successful registration", function (){
+            return request(server)
+              .post('/api/users/register')
+              .send(testUser)
+              .then(res => [
+                  expect(res.status).toBe(201)
+              ])
+        })
+    })
 
     
+
 
 
 
